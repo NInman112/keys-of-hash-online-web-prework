118f575e6ee8613f6db25379ae3e1f7cf7f2755d
Nick Inman <nick@theoatmeal.com>
2018-11-25 00:55:18 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,16 @@
+require"pry"
+
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    
+    new_array = []
+    each do |animal, location|
+      if arguments.include?(location)
+        new_array << animal
+           #binding.pry
+         end
+      end
+      new_array
   end
 end
